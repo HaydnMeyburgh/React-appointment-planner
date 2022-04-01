dc5d45b93305b4ddaf71bc37b9c91e9ee3d6df66
Haydn Meyburgh <haydnmey@gmail.com>
2022-04-01 10:13:48 +0200

    added functions for adding contacts and appointments to states

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import React from "react";
+import React, { useState} from "react";
 import { Switch, Route, Redirect, NavLink } from "react-router-dom";
-
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
 import { ContactsPage } from "./containers/contactsPage/ContactsPage";
 
 function App() {
-  /*
-  Define state variables for 
-  contacts and appointments 
-  */
-
   const ROUTES = {
     CONTACTS: "/contacts",
     APPOINTMENTS: "/appointments",
@@ -19,6 +13,28 @@ function App() {
   Implement functions to add data to
   contacts and appointments
   */
+  const [contacts, setContacts] = useState([{}]);
+  const addContact = (name, email, number) => {
+    setContacts((prev) => ({
+      ...prev,
+      Name: name,
+      Email: email,
+      Number: number
+    }));
+  };
+  // console.log(addContact)
+
+  const [appointments, setAppointments] = useState([{}]);
+  const addAppointment = (title, contact, time) => {
+    setAppointments((prev) => ({
+      ...prev,
+      Title: title,
+      Contact: contact,
+      Date: new Date(),
+      Time: time
+    }));
+  };
+  // console.log(addAppointment)
 
   return (
     <>
@@ -36,12 +52,16 @@ function App() {
             <Redirect to={ROUTES.CONTACTS} />
           </Route>
           <Route path={ROUTES.CONTACTS}>
-             {/* Add props to ContactsPage */}
-            <ContactsPage />
+            <ContactsPage 
+              contacts={contacts}
+              addContact={addContact}
+              />
           </Route>
           <Route path={ROUTES.APPOINTMENTS}>
-            {/* Add props to AppointmentsPage */}
-            <AppointmentsPage />
+            <AppointmentsPage 
+              appointments={appointments}
+              addAppointment={addAppointment}
+              />
           </Route>
         </Switch>
       </main>
