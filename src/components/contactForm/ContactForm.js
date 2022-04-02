e4c302523e6b45bf2282e3d0b65f859dffe1df1d
Haydn Meyburgh <haydnmey@gmail.com>
2022-04-02 11:03:16 +0200

    Created the form inputs with related attributes

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,6 +10,27 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    ContactForm
+    <form onSubmit={handleSubmit}>
+      <input placeholder="Name" 
+        type='text' 
+        name="name"
+        value={name} 
+        onChange={e => setName(e.target.value)}
+      />
+      <input placeholder="Email" 
+        type='email' 
+        name="email" 
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+      <input placeholder="Phone Number" 
+        type='number' 
+        name="phone" 
+        value={phone} 
+        pattern="^(27|0)[0-9]{9}"
+        onChange={e => setPhone(e.target.value)} 
+      />
+      <button>Submit</button>
+    </form>
   );
 };
