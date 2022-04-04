dbbdd8ff5d35f2f586820e2ab80fff786e62e8a9
Haydn Meyburgh <haydnmey@gmail.com>
2022-04-04 11:41:11 +0200

    mapped contacts and passed props to Tile component

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,14 @@
 import React from "react";
+import {Tile} from '../tile/Tile.js';
 
-export const TileList = () => {
+export const TileList = ({info}) => {
   return (
     <div>
-      
+      {info.map((contact, index) => (
+        <Tile key={index} 
+          value={contact} 
+        />
+      ))}
     </div>
   );
 };
