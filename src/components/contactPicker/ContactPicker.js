2374c3cfe20e05c76b36920cb3d80d9912a4c2cc
Haydn Meyburgh <haydnmey@gmail.com>
2022-04-04 17:54:36 +0200

    create a select drop down to pick from contacts that have been saved

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-export const ContactPicker = () => {
+export const ContactPicker = ({contacts, onChange}) => {
   return (
-    ContactPicker
+    <div>
+      <select onChange={onChange}>
+        <option selected='selected' key={-1} value='Please select'>Please Select Your contact details</option>
+        {contacts.map((contact, index) => (
+          <option key={contact[index]} value={contact.name}>{contact.name}</option>
+        ))}
+      </select>
+    </div>
   );
 };
