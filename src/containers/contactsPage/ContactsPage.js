1b491b4ec036ab5d37c4316fb3e7b09607d663a7
Haydn Meyburgh <haydnmey@gmail.com>
2022-04-04 11:42:53 +0200

    fixed issue where contact details were not being captured by App component

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,63 +1,64 @@
 import React, { useState, useEffect } from "react";
-import {ContactForm} from '../../components/contactForm/ContactForm.js';
-import {TileList} from '../../components/tileList/TileList.js';
-export const ContactsPage = ({contacts, addContacts}) => {
+import { ContactForm } from '../../components/contactForm/ContactForm.js';
+import { TileList } from '../../components/tileList/TileList.js';
+
+export const ContactsPage = (props) => {
   /*
   Define state variables for 
   contact info and duplicate check
   */
+  const contacts = props.contacts;
+  const addContacts = props.addContact;
+
   const [name, setName] = useState('');
-  const [phone, setPhone] = useState(null);
+  const [phone, setPhone] = useState('');
   const [email, setEmail] = useState('');
   const [duplicateCheck, setDuplicateCheck] = useState(false);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
     /*
     Add contact info and clear data
     if the contact name is not a duplicate
     */
-   if (duplicateCheck) return;
-    addContacts(name, phone, email);
-    setName('');
-    setPhone(null);
-    setEmail('');
+    if (duplicateCheck === false) {
+      addContacts(name, phone, email);
+      setEmail('');
+      setName('');
+      setPhone('');
+    } 
   };
-
   /*
   Using hooks, check for contact name in the 
   contacts array variable in props
   */
   useEffect(() => {
-    if (name === contacts.name) {
-      alert('This name is a duplicate, please use another');
-      setDuplicateCheck(true);
+    for (const contact of contacts){
+      if (name === contact.name) {
+        alert('This name is a duplicate, please use another');
+        setDuplicateCheck(true);
+      }
     }
-    setName(name);
-    setPhone(phone);
-    setEmail(email);
-  }, [name, contacts.name, email, phone]);
+  }, [name, contacts]);
 
   return (
     <div>
       <section>
         <h2>Add Contact</h2>
-        <ContactForm 
+        <ContactForm  
           name={name}
-          phone={phone}
-          email={email}
-          duplicateCheck={duplicateCheck}
-          setEmail={setEmail}
           setName={setName}
+          phone={phone}
           setPhone={setPhone}
-          setDuplicateCheck={setDuplicateCheck}
-          onSubmit={handleSubmit}
+          email={email}
+          setEmail={setEmail}
+          handleSubmit={handleSubmit}
         /> 
       </section>
       <hr />
       <section>
         <h2>Contacts</h2>
-        <TileList contacts={contacts}/>
+        <TileList info={contacts}/>
       </section>
     </div>
   );
