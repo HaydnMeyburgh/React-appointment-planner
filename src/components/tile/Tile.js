ca3c5a79f719e6396a53f3930bf94d907d6632c3
Haydn Meyburgh <haydnmey@gmail.com>
2022-04-04 11:40:03 +0200

    turned object into an array so that it can be mapped and to display contact details after they have been submitted

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({value}) => {
+
+  const arr = Object.values(value)
   return (
     <div className="tile-container">
-      
+      {arr.map((values, index) => {
+        if (index === 0) {
+          return  <p className="tile-title" key={index}>{values}</p>;
+        } else {
+          return <p className="tile" key={index}>{values}</p>;
+        }
+      })}
     </div>
   );
 };
